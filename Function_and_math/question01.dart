99031e50c281ab69903d4bb8d499d2d88aa132bf
Pawan Kumar <pawan630703@gmail.com>
2024-03-16 11:28:13 +0530

    dart 03

---
+++ Function_and_math/question01.dart
@@ -0,0 +1,8 @@
+void myName() {
+  print("Pawan Kumar");
+}
+
+void main() {
+  // Calling the function to display name
+  myName();
+}
---
+++ Function_and_math/question05.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+double pi = 3.14;
+void main() {
+
+  // required 
+  double area(double r) =>pi * pow(r, 2);
+
+  print(area(5));
+}
