32467088f3c7b35973dcc1a69369b61fb7725e35
Pawan Kumar <104499385+PawanKumar85@users.noreply.github.com>
2024-03-17 13:56:48 +0530

    Delete Collection directory

--- Collection/question01.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  List<String> names = ['Pawan', 'Anamika', 'Ujjwal'];
-
-  names.forEach((element) {
-    print(element);
-  });
-}
--- Collection/question02.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  Set<String> fruits = {"apple", "banana", "orange"};
-
-  fruits.forEach((element) {
-    print(element);
-  });
-}
